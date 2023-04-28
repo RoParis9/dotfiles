a85065334c42f55188b81f490a2e242be3c54db5
Rodrigo <rocostaparis@hotmail.com>
2023-04-28 00:26:45 -0300

    backing up config

--- init.el
+++ init.el
@@ -95,6 +95,8 @@
 
     ;;highlight parenteses
     (show-paren-mode 1)
+    (setq-default fill-column 80)
+
 
     ;;font size
     (set-face-attribute
@@ -133,7 +135,7 @@
 (use-package doom-themes
   :ensure t
   :config
-  (load-theme 'doom-material-dark t))
+  (load-theme 'doom-monokai-pro t))
 
   ;;acario-light the best light theme
 
@@ -247,39 +249,9 @@
 (use-package marginalia
    :ensure t
    :custom
-   (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+   (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light t))
    :init
    (marginalia-mode))
-
-;;completion
-(use-package corfu
- :ensure t
- ;; Optional customizations
- :custom
- (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
- (corfu-auto-prefix 1)
- (corfu-auto t)                 ;; Enable auto completion
- (corfu-auto-delay 0.0)
- (corfu-separator ?\s)          ;; Orderless field separator
-;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-;; (corfu-preview-current nil)    ;; Disable current candidate preview
-;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
- (corfu-scroll-margin 5)        ;; Use scroll margin
- (tab-always-indent 'complete)
-
-;; Enable Corfu only for certain modes.
-;; :hook ((prog-mode . corfu-mode)
-;;        (shell-mode . corfu-mode)
-;;        (eshell-mode . corfu-mode))
-
-;; Recommended: Enable Corfu globally.
-;; This is recommended since Dabbrev can be used globally (M-/).
-;; See also `corfu-excluded-modes'.
-:init
-(global-corfu-mode)
-(corfu-history-mode))
 
 (use-package kind-icon
   :ensure t
@@ -288,6 +260,46 @@
   (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
   :config
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+(use-package corfu
+    ;; Optional customizations
+     :custom
+     (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+     (corfu-auto t)                 ;; Enable auto completion
+     (corfu-separator ?\s)          ;; Orderless field separator
+     (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+     (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+     (corfu-preview-current nil)    ;; Disable current candidate preview
+     (corfu-preselect 'prompt)      ;; Preselect the prompt
+     (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+     (corfu-scroll-margin 5)        ;; Use scroll margin
+
+    ;; Enable Corfu only for certain modes.
+    ;; :hook ((prog-mode . corfu-mode)
+    ;;        (shell-mode . corfu-mode)
+    ;;        (eshell-mode . corfu-mode))
+
+    ;; Recommended: Enable Corfu globally.
+    ;; This is recommended since Dabbrev can be used globally (M-/).
+    ;; See also `corfu-exclude-modes'.
+
+    :init
+    (global-corfu-mode))
+
+;; extra configs
+(use-package emacs
+  :init
+  ;; TAB cycle if there are only few candidates
+  (setq completion-cycle-threshold 3)
+
+  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (setq tab-always-indent 'complete))
 
 (defun efs/org-mode-setup ()
   (org-indent-mode)
@@ -483,6 +495,18 @@
    (add-hook 'java-mode-hook 'flycheck-mode)
    (require 'dap-java))
 
+(require 'csharp-mode)
+
+(add-hook 'csharp-mode-hook #'omnisharp-mode)
+(add-hook 'csharp-mode-hook #'csharp-tree-sitter-mode)
+(add-hook 'csharp-mode-hook #'lsp)
+(add-hook 'csharp-mode-hook #'flycheck-mode)
+(add-hook 'csharp-mode-hook #'smartparens-mode)
+
+(use-package omnisharp
+   :ensure t
+   :after csharp-mode)
+
 (use-package tuareg
      :ensure t
      :mode (("\\.ocamlinit\\'" . tuareg-mode)))
@@ -541,7 +565,7 @@
 
 (use-package tide
   :ensure t
-  :after (js2-mode typescript-mode corfu flycheck)
+  :after (js2-mode typescript-mode corfu-mode flycheck)
   :hook ((typescript-mode . tide-setup)
          (typescript-mode . tide-hl-identifier-mode)
          (before-save . tide-format-before-save)))
